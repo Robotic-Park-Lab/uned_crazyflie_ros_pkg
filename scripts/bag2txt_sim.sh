8ff52f218f867b4399d08b5937018bf010dd2282
FranciscoJManasAlvarez <fma527@ual.es>
2020-11-10 13:13:06 +0100

    Update bag2txt.sh

--- scripts/bag2txt_sim.sh
+++ scripts/bag2txt_sim.sh
@@ -4,147 +4,291 @@ echo "-------------------------"
 
 file=$1
 ID_dataset=$2
-
+if !(test -d "$ID_dataset")
+then
 mkdir $ID_dataset
+else
+echo "Carpeta existente"
+fi
 
-echo "Topic (1/32): /clock"
+echo "Topic (1/28): /clock"
 echo "..."
 aux="_clock"
-rostopic echo -b $file -p /clock >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /clock > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (2/32): /crazyflie2/command/motor_speed"
+echo "Topic (2/28): /crazyflie2/command/motor_speed"
 echo "..."
 aux="_Cf2_cmd_mspeed"
-rostopic echo -b $file -p /crazyflie2/command/motor_speed >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/command/motor_speed > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (3/32): /crazyflie2/gazebo/command/motor_speed "
+echo "Topic (3/28): /crazyflie2/gazebo/command/motor_speed "
 echo "..."
 aux="_Cf2_gaz_cmd_mspeed"
-rostopic echo -b $file -p /crazyflie2/gazebo/command/motor_speed >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/gazebo/command/motor_speed > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (4/32): /crazyflie2/ground_truth/imu"
+echo "Topic (4/28): /crazyflie2/ground_truth/imu"
 echo "..."
 aux="_Cf2_gt_imu"
-rostopic echo -b $file -p /crazyflie2/ground_truth/imu >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/ground_truth/imu > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (5/32): /crazyflie2/ground_truth/odometry"
+echo "Topic (5/28): /crazyflie2/ground_truth/odometry"
 echo "..."
 aux="_Cf2_gt_odom"
-rostopic echo -b $file -p /crazyflie2/ground_truth/odometry >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/ground_truth/odometry > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (6/32): /crazyflie2/ground_truth/pose"
+echo "Topic (6/28): /crazyflie2/ground_truth/pose"
 echo "..."
 aux="_Cf2_gt_pose"
-rostopic echo -b $file -p /crazyflie2/ground_truth/pose >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/ground_truth/pose > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (7/32): /crazyflie2/ground_truth/pose_with_covariance"
+echo "Topic (7/28): /crazyflie2/ground_truth/pose_with_covariance"
 echo "..."
 aux="_Cf2_gt_posecov"
-rostopic echo -b $file -p /crazyflie2/ground_truth/pose_with_covariance >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/ground_truth/pose_with_covariance > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (8/32): /crazyflie2/ground_truth/position"
+echo "Topic (8/28): /crazyflie2/ground_truth/position"
 echo "..."
 aux="_Cf2_gt_pos"
-rostopic echo -b $file -p /crazyflie2/ground_truth/position >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/ground_truth/position > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (9/32): /crazyflie2/ground_truth/transform "
+echo "Topic (9/28): /crazyflie2/ground_truth/transform "
 echo "..."
 aux="_Cf2_gt_transf"
-rostopic echo -b $file -p /crazyflie2/ground_truth/transform >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/ground_truth/transform > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (10/32): /crazyflie2/joint_states"
+echo "Topic (10/28): /crazyflie2/joint_states"
 echo "..."
 aux="_Cf2_js"
-rostopic echo -b $file -p /crazyflie2/joint_states >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/joint_states > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (11/32): /crazyflie2/motor_speed"
+echo "Topic (11/28): /crazyflie2/motor_speed"
 echo "..."
 aux="_Cf2_mspeed"
-rostopic echo -b $file -p /crazyflie2/motor_speed >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/motor_speed > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (12/32): /crazyflie2/odometry"
+echo "Topic (12/28): /crazyflie2/odometry"
 echo "..."
 aux="_Cf2_odom"
-rostopic echo -b $file -p /crazyflie2/odometry >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/odometry > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (13/32): /crazyflie2/odometry_sensor1/odometry"
+echo "Topic (13/28): /crazyflie2/odometry_sensor1/odometry"
 echo "..."
 aux="_Cf2_odoms1_odom"
-rostopic echo -b $file -p /crazyflie2/odometry_sensor1/odometry >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/odometry_sensor1/odometry > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (14/32): /crazyflie2/odometry_sensor1/pose"
+echo "Topic (14/28): /crazyflie2/odometry_sensor1/pose"
 echo "..."
 aux="_Cf2_odoms1_pose"
-rostopic echo -b $file -p /crazyflie2/odometry_sensor1/pose >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/odometry_sensor1/pose > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (15/32): /crazyflie2/odometry_sensor1/pose_with_covariance"
+echo "Topic (15/28): /crazyflie2/odometry_sensor1/pose_with_covariance"
 echo "..."
 aux="_Cf2_odoms1_posecov"
-rostopic echo -b $file -p /crazyflie2/odometry_sensor1/pose_with_covariance >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/odometry_sensor1/pose_with_covariance > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (16/32): /crazyflie2/odometry_sensor1/position"
+echo "Topic (16/28): /crazyflie2/odometry_sensor1/position"
 echo "..."
 aux="_Cf2_odoms1_pos"
-rostopic echo -b $file -p /crazyflie2/odometry_sensor1/position >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/odometry_sensor1/position > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (17/32): /crazyflie2/odometry_sensor1/transform "
+echo "Topic (17/28): /crazyflie2/odometry_sensor1/transform "
 echo "..."
 aux="_Cf2_odoms1_transf"
-rostopic echo -b $file -p /crazyflie2/odometry_sensor1/transform >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/odometry_sensor1/transform > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (18/32): /crazyflie2/orientation_rpy"
+echo "Topic (18/28): /crazyflie2/orientation_rpy"
 echo "..."
 aux="_Cf2_rpy"
-rostopic echo -b $file -p /crazyflie2/orientation_rpy >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/orientation_rpy > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (19/32): /crazyflie2/pose"
+echo "Topic (19/28): /crazyflie2/pose"
 echo "..."
 aux="_Cf2_pose"
-rostopic echo -b $file -p /crazyflie2/pose >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/pose > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (20/32): /crazyflie2/pose_with_covariance"
+echo "Topic (20/28): /crazyflie2/pose_with_covariance"
 echo "..."
 aux="_Cf2_posecov"
-rostopic echo -b $file -p /crazyflie2/pose_with_covariance >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/pose_with_covariance > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (21/32): /crazyflie2/position"
+echo "Topic (21/28): /crazyflie2/position"
 echo "..."
 aux="_Cf2_pos"
-rostopic echo -b $file -p /crazyflie2/position >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/position > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (22/32): /crazyflie2/transform"
+echo "Topic (22/28): /crazyflie2/transform"
 echo "..."
 aux="_Cf2_transf"
-rostopic echo -b $file -p /crazyflie2/transform >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /crazyflie2/transform > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (23/32): /gazebo/link_states"
+echo "Topic (23/28): /gazebo/link_states"
 echo "..."
 aux="_gaz_lstates"
-rostopic echo -b $file -p /gazebo/link_states >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /gazebo/link_states > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (24/32): /gazebo/model_states"
+echo "Topic (24/28): /gazebo/model_states"
 echo "..."
 aux="_gaz_mstates"
-rostopic echo -b $file -p /gazebo/model_states >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /gazebo/model_states > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (25/32): /gazebo/parameter_descriptions"
+echo "Topic (25/28): /gazebo/parameter_descriptions"
 echo "..."
 aux="_gaz_param"
-rostopic echo -b $file -p /gazebo/parameter_descriptions >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /gazebo/parameter_descriptions > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (26/32): /gazebo/parameter_updates"
+echo "Topic (26/28): /gazebo/parameter_updates"
 echo "..."
 aux="_gaz_paramupdate"
-rostopic echo -b $file -p /gazebo/parameter_updates >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /gazebo/parameter_updates > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (27/32): /tf"
+echo "Topic (27/28): /tf"
 echo "..."
 aux="_tf"
-rostopic echo -b $file -p /tf >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /tf > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
-echo "Topic (28/32): /tf_static"
+echo "Topic (28/28): /tf_static"
 echo "..."
 aux="_tf_static"
-rostopic echo -b $file -p /tf_static >> $ID_dataset/$ID_dataset$aux.txt
+if !(test -f "$ID_dataset/$ID_dataset$aux.txt")
+then
+rostopic echo -b $file -p /tf_static > $ID_dataset/$ID_dataset$aux.txt
+else
+echo "Archivo existente"
+fi
 
 echo "Proceso Finalizado"
