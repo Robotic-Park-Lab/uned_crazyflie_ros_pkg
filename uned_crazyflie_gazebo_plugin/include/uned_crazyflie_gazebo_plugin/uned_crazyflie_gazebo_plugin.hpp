1d151fb92d391d97bd587ed79fedc0f45f653cd6
FranciscoJManasAlvarez <fma527@ual.es>
2021-08-14 12:48:28 +0200

    gazebo_plugin

---
+++ uned_crazyflie_gazebo_plugin/include/uned_crazyflie_gazebo_plugin/uned_crazyflie_gazebo_plugin.hpp
@@ -0,0 +1,12 @@
+// FILE
+
+#ifndef _CRAZYFLIE2_PLUGIN_HH_
+#define _CRAZYFLIE2_PLUGIN_HH_
+
+#include <gazebo/common/PID.hh>
+#include <gazebo/common/Plugin.hh>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+
+
+#endif 
---
+++ uned_crazyflie_gazebo_plugin/src/uned_crazyflie_gazebo_plugin.cpp
@@ -0,0 +1,5 @@
+#include "uned_crazyflie_gazebo_plugin/uned_crazyflie_gazebo_plugin.hpp"
+
+#include <gazebo/physics/physics.hh>
+#include <iostream>
+#include <gazebo_ros/node.hpp>
