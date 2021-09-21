df16de9da403debd835e3398d7ac6a90397614ee
Robotic Park <lab.nanodrones@dia.uned.es>
2021-09-21 11:57:30 +0200

    Init new crazyflie driver. Python-ROS2

---
+++ uned_crazyflie_driver/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'uned_crazyflie_driver'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Francisco José Mañas Álvarez',
+    maintainer_email='fjmanas@dia.uned.es',
+    description='Paquete para el manejo de los nanodrones crazyflie 2.1',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'crazyflie_driver = uned_crazyflie_driver.crazyflie_driver:main'
+        ],
+    },
+)
---
+++ uned_crazyflie_driver/uned_crazyflie_driver/crazyflie_driver.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from uned_crazyflie_driver.')
+
+
+if __name__ == '__main__':
+    main()
