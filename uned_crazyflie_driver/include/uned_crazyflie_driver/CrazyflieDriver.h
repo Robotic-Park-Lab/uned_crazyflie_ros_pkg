b8d372aedf75ba1ed791256e5852d775ac49aeca
FranciscoJManasAlvarez <fma527@ual.es>
2021-11-23 11:01:18 +0100

    Update Sim driver

--- uned_crazyflie_driver/include/uned_crazyflie_driver/CrazyflieDriver.h
+++ uned_crazyflie_driver/include/uned_crazyflie_driver/CrazyflieDriver.h
@@ -15,6 +15,7 @@
 #include <std_msgs/String.h>
 #include <geometry_msgs/Pose.h>
 #include <mav_msgs/Actuators.h>
+#include <uned_crazyflie_controllers/AttitudeRefs.h>
 
 class CrazyflieDriverSim
 {
@@ -29,7 +30,7 @@ class CrazyflieDriverSim
 	ros::NodeHandle m_nh{};
 	ros::NodeHandle m_nh_params{"~"};
 
-	ros::Publisher m_pub_cmdcontrol;
+	ros::Publisher m_pub_cmdcontrol, m_pub_control_signal, m_pub_omega, m_pub_dyaw;
 
 	ros::Subscriber m_sub_onboard, m_sub_cmd_motors;
 
@@ -42,6 +43,8 @@ class CrazyflieDriverSim
      void onboardCallback(const std_msgs::Float64MultiArray::ConstPtr& msg);
      void cmdcontrolCallback(const std_msgs::Float64MultiArray::ConstPtr& msg);
      void rotorvelocitiesCallback(const Eigen::Vector4d rotor_velocities);
+     void attitudeRateMixerRefsCallback(const double omega, const double pitch, const double roll, const double dyaw);
+
 
      std_msgs::String m_controller_type, m_robot_id, m_controller_mode;
      Eigen::Vector4d ref_rotor_velocities;
--- uned_crazyflie_driver/src/uned_crazyflie_driver/uned_crazyflie_driver.cpp
+++ uned_crazyflie_driver/src/uned_crazyflie_driver/uned_crazyflie_driver.cpp
@@ -6,6 +6,10 @@ bool CrazyflieDriverSim::initialize()
 
   // Publisher:
   m_pub_cmdcontrol = m_nh.advertise<mav_msgs::Actuators>("command/motor_speed", 10);
+	m_pub_control_signal = m_nh.advertise<uned_crazyflie_controllers::AttitudeRefs>("attitude_controller_ref", 10);
+	m_pub_dyaw = m_nh.advertise<std_msgs::Float64>("dyaw_controller_ref", 10);
+	m_pub_omega = m_nh.advertise<std_msgs::Float64>("omega_signal", 10);
+
 	// Subscriber:
 	// Reference
 	m_sub_onboard = m_nh.subscribe( "onboard_cmd", 10, &CrazyflieDriverSim::onboardCallback, this);
@@ -24,9 +28,14 @@ bool CrazyflieDriverSim::iterate()
     ref_rotor_velocities[3] = m_cmd_motors.data[3];
     rotorvelocitiesCallback(ref_rotor_velocities);
   }
-  if(onboard){
-    // TO-DO. Insert Attitude & Rate Controller
-  }
+	if(onboard){
+		std_msgs::Float64 msg_omega;
+		msg_omega.data = thrust;
+		m_pub_omega.publish(msg_omega);
+		std_msgs::Float64 msg_dyaw;
+		msg_dyaw.data = 0.0;
+		m_pub_dyaw.publish(msg_dyaw);
+	}
 	return true;
 }
 
@@ -36,12 +45,15 @@ void CrazyflieDriverSim::onboardCallback(const std_msgs::Float64MultiArray::Cons
   pitch = msg->data[2];
   yaw = msg->data[3];
   onboard = true;
+	motors = false;
+	attitudeRateMixerRefsCallback(thrust, pitch, roll, yaw);
   ROS_INFO("Onboard Control: Thrust: %0.2f \tRoll: %0.2f \tPitch: %0.2f \tYaw: %0.2f", thrust, roll, pitch, yaw);
 }
 
 void CrazyflieDriverSim::cmdcontrolCallback(const std_msgs::Float64MultiArray::ConstPtr& msg){
   m_cmd_motors.data = msg->data;
   motors = true;
+	onboard = false;
   ROS_INFO("Motors Control: %0.2f \t%0.2f \t%0.2f \t%0.2f", m_cmd_motors.data[0], m_cmd_motors.data[1], m_cmd_motors.data[2], m_cmd_motors.data[3]);
 }
 
@@ -57,4 +69,15 @@ void CrazyflieDriverSim::rotorvelocitiesCallback(const Eigen::Vector4d rotor_vel
 	actuator_msg->header.stamp = ros::Time::now();
 
 	m_pub_cmdcontrol.publish(actuator_msg);
+}
+
+void CrazyflieDriverSim::attitudeRateMixerRefsCallback(const double omega, const double pitch, const double roll, const double dyaw)
+{
+	uned_crazyflie_controllers::AttitudeRefs ref_msg;
+
+	ref_msg.timestamp = ros::Time::now().toSec();
+	ref_msg.pitch = pitch;
+	ref_msg.roll = roll;
+
+	m_pub_control_signal.publish(ref_msg);
 }
