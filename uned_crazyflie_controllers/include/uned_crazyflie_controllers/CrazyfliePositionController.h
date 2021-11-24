89c69c03fdda9d679d677abb470b6014f38a7b54
FranciscoJManasAlvarez <fma527@ual.es>
2021-11-24 09:53:13 +0100

    Update Position controller

--- uned_crazyflie_controllers/include/uned_crazyflie_controllers/CrazyfliePositionController.h
+++ uned_crazyflie_controllers/include/uned_crazyflie_controllers/CrazyfliePositionController.h
@@ -11,8 +11,10 @@
 #include <chrono>
 #include <std_msgs/Bool.h>
 #include <std_msgs/Float64.h>
+#include <std_msgs/Float64MultiArray.h>
 #include <std_msgs/String.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Quaternion.h>
 #include <mav_msgs/Actuators.h>
 #include <uned_crazyflie_controllers/AttitudeRefs.h>
 
@@ -42,20 +44,89 @@ class CrazyfliePositionController
     void rotorvelocitiesCallback(const Eigen::Vector4d rotor_velocities);
     void attitudeRateMixerRefsCallback(const double omega, const double pitch, const double roll, const double dyaw);
     void positionreferenceCallback(const geometry_msgs::Pose::ConstPtr& msg);
+    struct pid_s{
+        double kp, ki, kd, td;
+        int nd;
+        double error[2], integral, derivative[2], upperlimit, lowerlimit;
+    };
+    struct euler_angles {
+        double roll, pitch, yaw;
+    };
+    euler_angles quaternion2euler(geometry_msgs::Quaternion quat) {
+        euler_angles rpy;
 
+        // roll (x-axis rotation)
+        double sinr_cosp = 2 * (quat.w * quat.x + quat.y * quat.z);
+        double cosr_cosp = 1 - 2 * (quat.x * quat.x + quat.y * quat.y);
+        rpy.roll = std::atan2(sinr_cosp, cosr_cosp) * (180 / 3.14159265);
+
+        // pitch (y-axis rotation)
+        double sinp = 2 * (quat.w * quat.y - quat.z * quat.x);
+        if (std::abs(sinp) >= 1)
+            rpy.pitch = std::copysign(3.14159265 / 2, sinp) * (180 / 3.14159265);
+        else
+            rpy.pitch = std::asin(sinp) * (180 / 3.14159265);
+
+        // yaw (z-axis rotation)
+        double siny_cosp = 2 * (quat.w * quat.z + quat.x * quat.y);
+        double cosy_cosp = 1 - 2 * (quat.y * quat.y + quat.z * quat.z);
+        rpy.yaw = std::atan2(siny_cosp, cosy_cosp) * (180 / 3.14159265);
+
+        return rpy;
+    }
+    double pid_controller(struct pid_s controller, double dt){
+        double outP = controller.kp * controller.error[0];
+        controller.integral = controller.integral + controller.ki * controller.error[1] * dt;
+        controller.derivative[0] = (controller.td/(controller.td+controller.nd+dt))*controller.derivative[1]+(controller.kd*controller.nd/(controller.td+controller.nd*dt))*(controller.error[0]-controller.error[1]);
+        double out = outP + controller.integral + controller.derivative[0];
+
+        double out_i = out;
+
+        if (out > controller.upperlimit)
+            out = controller.upperlimit;
+        if (out < controller.lowerlimit)
+            out = controller.lowerlimit;
+
+        controller.integral = controller.integral - (out - out_i) * sqrt(controller.kp / controller.ki);
+
+        controller.error[1] = controller.error[0];
+        controller.derivative[1] = controller.derivative[0];
+
+        return out;
+    }
+    struct pid_s init_controller(const char id[], double kp, double ki, double kd, double td, int nd, double upperlimit, double lowerlimit){
+        struct pid_s controller;
+
+        controller.kp = kp;
+        controller.ki = ki;
+        controller.kd = kd;
+        controller.td = td;
+        controller.nd = nd;
+        controller.error[0] = 0.0;
+        controller.error[1] = 0.0;
+        controller.integral = 0.0;
+        controller.derivative[0] = 0.0;
+        controller.derivative[1] = 0.0;
+        controller.upperlimit = upperlimit;
+        controller.lowerlimit = lowerlimit;
+
+        ROS_INFO("%s Controller: kp: %0.2f \tki: %0.2f \tkd: %0.2f", id, controller.kp, controller.ki, controller.kd);
+        return controller;
+  }
+
+    double dt = 0.01;
     double m_x_init, m_y_init, m_z_init;
-    std::string m_controller_type, m_robot_id, m_controller_mode;
-    geometry_msgs::Pose m_GT_pose, m_ref_pose, m_ref_position, m_ref_test;
-
+    std::string m_controller_type, m_robot_id, m_controller_mode, str_id;
+    geometry_msgs::Pose GT_pose, ref_pose;
+    bool first_pose_received = false;
+    bool first_ref_received = false;
     // Controllers
-    double Z_q[3], X_q[3], Y_q[3], U_q[3], V_q[3], Yaw_q[3];
-    // Altitude paremeters
-    double z_error_signal[3], delta_omega[2];
-    double omega = 0.0;
-    double we = 16073.0; //14480.0;
+    struct pid_s z_controller, w_controller, x_controller, u_controller, y_controller, v_controller;
+    // Altitude Controller
+    double w_feedback[2], w_signal, w_ref, thrust;
     // X-Y paremeters
-    double x_error_signal[3], y_error_signal[3], uc[2], vc[2], u_feedback[2], v_feedback[2];
-    double u_error_signal[3], v_error_signal[3], pitch_ref[2], roll_ref[2];
-    // Yaw Controller
-    double yaw_error_signal[3], dyaw_ref[2];
+    double x_global_error, y_global_error, u_ref, v_ref, u_feedback[2], v_feedback[2];
+    double u_signal, v_signal, pitch, roll;
+    // Angles
+    struct euler_angles rpy_ref, rpy_state;
 };
--- uned_crazyflie_controllers/src/uned_crazyflie_controller/uned_crazyflie_position_controller.cpp
+++ uned_crazyflie_controllers/src/uned_crazyflie_controller/uned_crazyflie_position_controller.cpp
@@ -6,6 +6,7 @@ bool CrazyfliePositionController::initialize()
 	ROS_INFO("CrazyfliePositionController::inicialize() ok.");
 
 	// Lectura de parámetros
+	    ROS_INFO("TO-DO: Read Params.");
 	m_nh_params.getParam("CONTROLLER_TYPE", m_controller_type);
 	m_nh_params.getParam("ROBOT_ID", m_robot_id);
 	m_nh_params.getParam("CONTROLLER_MODE", m_controller_mode);
@@ -13,37 +14,24 @@ bool CrazyfliePositionController::initialize()
 	m_nh_params.getParam("Y_POS", m_y_init);
 	m_nh_params.getParam("Z_POS", m_z_init);
 
-
-	if(m_nh_params.hasParam("Zq1") && m_nh_params.hasParam("Zq2") && m_nh_params.hasParam("Zq3")){
-		m_nh_params.getParam("Zq1", Z_q[0]);
-		m_nh_params.getParam("Zq2", Z_q[1]);
-		m_nh_params.getParam("Zq3", Z_q[2]);
-	}
-	if(m_nh_params.hasParam("Xq1") && m_nh_params.hasParam("Xq2") && m_nh_params.hasParam("Xq3")){
-		m_nh_params.getParam("Xq1", X_q[0]);
-		m_nh_params.getParam("Xq2", X_q[1]);
-		m_nh_params.getParam("Xq3", X_q[2]);
-	}
-	if(m_nh_params.hasParam("Yq1") && m_nh_params.hasParam("Yq2") && m_nh_params.hasParam("Yq3")){
-		m_nh_params.getParam("Yq1", Y_q[0]);
-		m_nh_params.getParam("Yq2", Y_q[1]);
-		m_nh_params.getParam("Yq3", Y_q[2]);
-	}
-	if(m_nh_params.hasParam("Uq1") && m_nh_params.hasParam("Uq2") && m_nh_params.hasParam("Uq3")){
-		m_nh_params.getParam("Uq1", U_q[0]);
-		m_nh_params.getParam("Uq2", U_q[1]);
-		m_nh_params.getParam("Uq3", U_q[2]);
-	}
-	if(m_nh_params.hasParam("Vq1") && m_nh_params.hasParam("Vq2") && m_nh_params.hasParam("Vq3")){
-		m_nh_params.getParam("Vq1", V_q[0]);
-		m_nh_params.getParam("Vq2", V_q[1]);
-		m_nh_params.getParam("Vq3", V_q[2]);
-	}
-	if(m_nh_params.hasParam("Yawq1") && m_nh_params.hasParam("Yawq2") && m_nh_params.hasParam("Yawq3")){
-		m_nh_params.getParam("Yawq1", Yaw_q[0]);
-		m_nh_params.getParam("Yawq2", Yaw_q[1]);
-		m_nh_params.getParam("Yawq3", Yaw_q[2]);
-	}
+	// Z Controller
+	str_id = "Z";
+	z_controller = init_controller(str_id.c_str(), 2.0, 0.5, 0.0, 0.0, 100, 1.0, -1.0);
+	// W Controller
+	str_id = "W";
+	w_controller = init_controller(str_id.c_str(), 25.0, 15.0, 0.0, 0.0, 100, 1160.0, -640.0);
+	// X Controller
+	str_id = "X";
+	x_controller = init_controller(str_id.c_str(), 2.0, 0.5, 0.0, 0.0, 100, 1.0, -1.0);
+	// U Controller
+	str_id = "U";
+	u_controller = init_controller(str_id.c_str(), 25.0, 2.0, 0.0, 0.0, 100, 30.0, -30.0);
+	// Y Controller
+	str_id = "Y";
+	y_controller = init_controller(str_id.c_str(), 2.0, 0.5, 0.0, 0.0, 100, 1.0, -1.0);
+	// V Controller
+	str_id = "V";
+	v_controller = init_controller(str_id.c_str(), 25.0, 2.0, 0.0, 0.0, 100, 30.0, -30.0);
 	// Publisher:
 	// Referencias para los controladores PID Attitude y Rate
 	m_pub_control_signal = m_nh.advertise<uned_crazyflie_controllers::AttitudeRefs>("attitude_controller_ref", 10);
@@ -60,148 +48,79 @@ bool CrazyfliePositionController::initialize()
 	u_feedback[0] = m_x_init;
 	v_feedback[0] = m_y_init;
 
-	m_ref_pose.position.x = m_x_init;
-	m_ref_pose.position.y = m_y_init;
-	m_ref_pose.position.z = m_z_init;
-	m_ref_pose.orientation.x = 0;
-	m_ref_pose.orientation.y = 0;
-	m_ref_pose.orientation.z = 0;
-	m_ref_pose.orientation.w = 1;
-
-	ROS_INFO("In progress ...");
-	ROS_INFO_STREAM("Goal Pose: " << m_ref_pose);
-	ROS_INFO("Altitude PID(Z) Parameters: \t%f \t%f \t%f", Z_q[0], Z_q[1], Z_q[2]);
-	ROS_INFO("X PID(Z) Parameters: \t%f \t%f \t%f", X_q[0], X_q[1], X_q[2]);
-	ROS_INFO("U PID(Z) Parameters: \t%f \t%f \t%f", U_q[0], U_q[1], U_q[2]);
-	ROS_INFO("Y PID(Z) Parameters: \t%f \t%f \t%f", Y_q[0], Y_q[1], Y_q[2]);
-	ROS_INFO("V PID(Z) Parameters: \t%f \t%f \t%f", V_q[0], V_q[1], V_q[2]);
-	ROS_INFO("Yaw PID(Z) Parameters: \t%f \t%f \t%f", Yaw_q[0], Yaw_q[1], Yaw_q[2]);
-	ROS_ERROR("Hay que esperar a que gazebo esté listo para lanzar el nodo");
-
 	return true;
 }
 
 bool CrazyfliePositionController::iterate()
 {
-	// Altitude Controller
-	{
-		// Update error vector
-		z_error_signal[2] = z_error_signal[1];
-		z_error_signal[1] = z_error_signal[0];
-		z_error_signal[0] = m_ref_pose.position.z - m_GT_pose.position.z;
+	if (first_pose_received && first_ref_received) {
+		// Z Controller
+		z_controller.error[0] = ref_pose.position.z - GT_pose.position.z;
+		w_ref = pid_controller(z_controller, dt);
 
-		// Update signal vector
-		delta_omega[1] = delta_omega[0];
-		delta_omega[0] = delta_omega[1] + Z_q[0]*z_error_signal[0] + Z_q[1]*z_error_signal[1] + Z_q[2]*z_error_signal[2];
+		// W Controller
+		w_feedback[1] = w_feedback[0];
+		w_feedback[0] = GT_pose.position.z;
+		w_signal = (w_feedback[0] - w_feedback[1]) / dt;
+		w_controller.error[0] = w_ref - w_signal;
+		thrust = pid_controller(w_controller, dt);
 
-		// Saturation
-		if(delta_omega[0]>15000)
-			delta_omega[0] = 15000;
-		if(delta_omega[0]<-15000)
-			delta_omega[0] = -15000;
+		thrust = thrust * 1000 + 36000;
 
-		// Output signal
-		omega = delta_omega[0]+(we-4070.3)/0.2685;
-		std_msgs::Float64 msg_omega;
-		msg_omega.data = omega;
-		m_pub_omega.publish(msg_omega);
-	}
-	// X-Y Controller
-	// Convert quaternion to yw
-	double siny_cosp_ref = 2 * (m_ref_pose.orientation.w*m_ref_pose.orientation.z+m_ref_pose.orientation.x*m_ref_pose.orientation.y);
-	double cosy_cosp_ref = 1 - 2 * (m_ref_pose.orientation.y*m_ref_pose.orientation.y + m_ref_pose.orientation.z*m_ref_pose.orientation.z);
-	double yaw_ref = std::atan2(siny_cosp_ref,cosy_cosp_ref);
-	double siny_cosp = 2 * (m_GT_pose.orientation.w*m_GT_pose.orientation.z+m_GT_pose.orientation.x*m_GT_pose.orientation.y);
-	double cosy_cosp = 1 - 2 * (m_GT_pose.orientation.y*m_GT_pose.orientation.y + m_GT_pose.orientation.z*m_GT_pose.orientation.z);
-	double yaw = std::atan2(siny_cosp,cosy_cosp);
-	{
-		// Position
-		// Update local error
-		x_error_signal[2] = x_error_signal[1];
-		x_error_signal[1] = x_error_signal[0];
-		x_error_signal[0] = (m_ref_pose.position.x - m_GT_pose.position.x)*cos(yaw)+(m_ref_pose.position.y - m_GT_pose.position.y)*sin(yaw);
-		y_error_signal[2] = y_error_signal[1];
-		y_error_signal[1] = y_error_signal[0];
-		y_error_signal[0] = -(m_ref_pose.position.x - m_GT_pose.position.x)*sin(yaw)+(m_ref_pose.position.y - m_GT_pose.position.y)*cos(yaw);
+		// Convert quaternion to yw
+		rpy_ref = quaternion2euler(ref_pose.orientation);
+		rpy_state = quaternion2euler(GT_pose.orientation);
 
-		// Update signal vector
-		uc[1] = uc[0];
-		uc[0] = uc[1] + X_q[0]*x_error_signal[0] + X_q[1]*x_error_signal[1] + X_q[2]*x_error_signal[2];
-		vc[1] = vc[0];
-		vc[0] = vc[1] + Y_q[0]*y_error_signal[0] + Y_q[1]*y_error_signal[1] + Y_q[2]*y_error_signal[2];
+		x_global_error = ref_pose.position.x - GT_pose.position.x;
+		y_global_error = ref_pose.position.y - GT_pose.position.y;
+		// X Controller
+		x_controller.error[0] = x_global_error * cos(rpy_state.yaw) + y_global_error * sin(rpy_state.yaw);
+		u_ref = pid_controller(x_controller, dt);
+		// Y Controller
+		y_controller.error[0] = -x_global_error * sin(rpy_state.yaw) + y_global_error * cos(rpy_state.yaw);
+		v_ref = pid_controller(y_controller, dt);
 
 		// Speed
 		u_feedback[1] = u_feedback[0];
-		u_feedback[0] = m_GT_pose.position.x;
-		double u_signal = (u_feedback[0]-u_feedback[1])/0.01;
+		u_feedback[0] = GT_pose.position.x;
+		u_signal = (u_feedback[0] - u_feedback[1]) / dt;
 		v_feedback[1] = v_feedback[0];
-		v_feedback[0] = m_GT_pose.position.y;
-		double v_signal = (v_feedback[0]-v_feedback[1])/0.01;
+		v_feedback[0] = GT_pose.position.y;
+		v_signal = (v_feedback[0] - v_feedback[1]) / dt;
 
-		// Update error
-		u_error_signal[2] = u_error_signal[1];
-		u_error_signal[1] = u_error_signal[0];
-		u_error_signal[0] = uc[0]-u_signal;
-		v_error_signal[2] = v_error_signal[1];
-		v_error_signal[1] = v_error_signal[0];
-		v_error_signal[0] = vc[0]-v_signal;
+		// U Controller
+		u_controller.error[0] = u_ref - u_signal;
+		pitch = pid_controller(u_controller, dt);
 
-		// Update signal vector
-		pitch_ref[1] = pitch_ref[0];
-		pitch_ref[0] = pitch_ref[1] + U_q[0]*u_error_signal[0] + U_q[1]*u_error_signal[1] + U_q[2]*u_error_signal[2];
-		roll_ref[1] = roll_ref[0];
-		roll_ref[0] = roll_ref[1] + V_q[0]*v_error_signal[0] + V_q[1]*v_error_signal[1] + V_q[2]*v_error_signal[2];
+		// V Controller
+		v_controller.error[0] = v_ref - v_signal;
+		roll = pid_controller(v_controller, dt);
 
-		// Saturation
-		if(pitch_ref[0]>30)
-			pitch_ref[0] = 30;
-		if(pitch_ref[0]<-30)
-			pitch_ref[0] = -30;
-		if(roll_ref[0]>30)
-			roll_ref[0] = 30;
-		if(roll_ref[0]<-30)
-			roll_ref[0] = -30;
+		attitudeRateMixerRefsCallback(thrust, pitch, roll, rpy_ref.yaw);
+
 	}
-	// Yaw Controller
-	{
-		// Update error
-		yaw_error_signal[2] = yaw_error_signal[1];
-		yaw_error_signal[1] = yaw_error_signal[0];
-		yaw_error_signal[0] = (yaw_ref-yaw)*180/3.14159265;
-
-		// Update signal vector
-		dyaw_ref[1] = dyaw_ref[0];
-		dyaw_ref[0] = dyaw_ref[1] + Yaw_q[0]*yaw_error_signal[0] + Yaw_q[1]*yaw_error_signal[1] + Yaw_q[2]*yaw_error_signal[2];
-		std_msgs::Float64 msg_dyaw;
-		msg_dyaw.data = dyaw_ref[0];
-		m_pub_dyaw.publish(msg_dyaw);
-	}
-
-	attitudeRateMixerRefsCallback(omega, pitch_ref[0], roll_ref[0], dyaw_ref[0]);
 
 	return true;
 }
 
-void CrazyfliePositionController::attitudeRateMixerRefsCallback(const double omega, const double pitch, const double roll, const double dyaw)
+void CrazyfliePositionController::attitudeRateMixerRefsCallback(const double omega, const double pitch, const double roll, const double yaw)
 {
-	uned_crazyflie_controllers::AttitudeRefs ref_msg;
+	std_msgs::Float64MultiArray msg_cmd;
 
-	ref_msg.timestamp = ros::Time::now().toSec();
-	ref_msg.pitch = pitch;
-	ref_msg.roll = roll;
+	msg_cmd.data = { thrust, roll, pitch, rpy_ref.yaw };
 
-	m_pub_control_signal.publish(ref_msg);
+	m_pub_control_signal.publish(msg_cmd);
 }
 
 void CrazyfliePositionController::positionreferenceCallback(const geometry_msgs::Pose::ConstPtr& msg)
 {
-	m_ref_pose.position = msg->position;
-	m_ref_pose.orientation = msg->orientation;
-	ROS_INFO("New Pose: x: %f \ty: %f \tz: %f", m_ref_pose.position.x, m_ref_pose.position.y, m_ref_pose.position.z);
+	ref_pose.position = msg->position;
+	ref_pose.orientation = msg->orientation;
+	ROS_INFO("New Pose: x: %f \ty: %f \tz: %f", ref_pose.position.x, ref_pose.position.y, ref_pose.position.z);
 }
 
 void CrazyfliePositionController::gtposeCallback(const geometry_msgs::Pose::ConstPtr& msg)
 {
-	m_GT_pose.position = msg->position;
-	m_GT_pose.orientation = msg->orientation;
+	GT_pose.position = msg->position;
+	GT_pose.orientation = msg->orientation;
 }
