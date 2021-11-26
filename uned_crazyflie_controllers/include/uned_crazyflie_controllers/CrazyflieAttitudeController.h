b6196a0cb2733964c845d1269bd7c7228fe08729
FranciscoJManasAlvarez <fma527@ual.es>
2021-11-26 20:12:04 +0100

    Clean Attitude Controller

--- uned_crazyflie_controllers/include/uned_crazyflie_controllers/CrazyflieAttitudeController.h
+++ uned_crazyflie_controllers/include/uned_crazyflie_controllers/CrazyflieAttitudeController.h
@@ -18,6 +18,15 @@
 #include <uned_crazyflie_controllers/AttitudeRefs.h>
 #include <uned_crazyflie_controllers/RateMixerRefs.h>
 
+struct pid_s{
+   double kp, ki, kd, td;
+   int nd;
+   double error[2], integral, derivative[2], upperlimit, lowerlimit;
+};
+struct euler_angles{
+   double roll, pitch, yaw;
+};
+
 class CrazyflieAttitudeController
 {
    public:
@@ -40,80 +49,14 @@ class CrazyflieAttitudeController
 	bool iterate();
 
    protected:
-    struct pid_s{
-       double kp, ki, kd, td;
-       int nd;
-       double error[2], integral, derivative[2], upperlimit, lowerlimit;
-    };
-    struct euler_angles{
-       double roll, pitch, yaw;
-    };
+
     void gtposeCallback(const geometry_msgs::Pose::ConstPtr& msg);
     void rotorvelocitiesCallback(const Eigen::Vector4d rotor_velocities);
     void attitudeRefsCallback(const uned_crazyflie_controllers::AttitudeRefs::ConstPtr& msg);
     void rateMixerRefsCallback(const double dpitch, const double droll, const double dyaw);
-
-    euler_angles quaternion2euler(geometry_msgs::Quaternion quat){
-        euler_angles rpy;
-
-        // roll (x-axis rotation)
-        double sinr_cosp = 2 * (quat.w * quat.x + quat.y * quat.z);
-        double cosr_cosp = 1 - 2 * (quat.x * quat.x + quat.y * quat.y);
-        rpy.roll = std::atan2(sinr_cosp, cosr_cosp) * (180 / 3.14159265);
-
-        // pitch (y-axis rotation)
-        double sinp = 2 * (quat.w * quat.y - quat.z * quat.x);
-        if (std::abs(sinp) >= 1)
-            rpy.pitch = std::copysign(3.14159265 / 2, sinp) * (180 / 3.14159265);
-        else
-            rpy.pitch = std::asin(sinp) * (180 / 3.14159265);
-
-        // yaw (z-axis rotation)
-        double siny_cosp = 2 * (quat.w * quat.z + quat.x * quat.y);
-        double cosy_cosp = 1 - 2 * (quat.y * quat.y + quat.z * quat.z);
-        rpy.yaw = std::atan2(siny_cosp, cosy_cosp) * (180 / 3.14159265);
-
-        return rpy;
-    }
-    double pid_controller(struct pid_s controller, double dt){
-        double outP = controller.kp * controller.error[0];
-        controller.integral = controller.integral + controller.ki * controller.error[1] * dt;
-        controller.derivative[0] = (controller.td/(controller.td+controller.nd+dt))*controller.derivative[1]+(controller.kd*controller.nd/(controller.td+controller.nd*dt))*(controller.error[0]-controller.error[1]);
-        double out = outP + controller.integral + controller.derivative[0];
-
-        double out_i = out;
-
-        if (out > controller.upperlimit)
-            out = controller.upperlimit;
-        if (out < controller.lowerlimit)
-            out = controller.lowerlimit;
-
-        controller.integral = controller.integral - (out - out_i) * sqrt(controller.kp / controller.ki);
-
-        controller.error[1] = controller.error[0];
-        controller.derivative[1] = controller.derivative[0];
-
-        return out;
-    }
-    struct pid_s init_controller(const char id[], double kp, double ki, double kd, double td, int nd, double upperlimit, double lowerlimit){
-        struct pid_s controller;
-
-        controller.kp = kp;
-        controller.ki = ki;
-        controller.kd = kd;
-        controller.td = td;
-        controller.nd = nd;
-        controller.error[0] = 0.0;
-        controller.error[1] = 0.0;
-        controller.integral = 0.0;
-        controller.derivative[0] = 0.0;
-        controller.derivative[1] = 0.0;
-        controller.upperlimit = upperlimit;
-        controller.lowerlimit = lowerlimit;
-
-        ROS_INFO("%s Controller: kp: %0.2f \tki: %0.2f \tkd: %0.2f", id, controller.kp, controller.ki, controller.kd);
-        return controller;
-    }
+    euler_angles quaternion2euler(geometry_msgs::Quaternion quat);
+    double pid_controller(struct pid_s controller, double dt);
+    struct pid_s init_controller(const char id[], double kp, double ki, double kd, double td, int nd, double upperlimit, double lowerlimit);
 
     // Controllers
     struct pid_s pitch_controller, roll_controller, yaw_controller;
@@ -127,6 +70,7 @@ class CrazyflieAttitudeController
     bool first_pose_received = false;
     bool first_ref_received = false;
     double dt = 0.002;
+    const double PI = 3.14159265;
     double kp, ki, kd, td;
 
 };
--- uned_crazyflie_controllers/src/uned_crazyflie_controller/uned_crazyflie_attitude_controller.cpp
+++ uned_crazyflie_controllers/src/uned_crazyflie_controller/uned_crazyflie_attitude_controller.cpp
@@ -47,9 +47,8 @@ bool CrazyflieAttitudeController::initialize()
 	return true;
 }
 
-bool CrazyflieAttitudeController::iterate()
-{
-
+bool CrazyflieAttitudeController::iterate(){
+	
 	if (first_ref_received && first_pose_received) {
 			ROS_INFO_ONCE("AttitudeRateController::iterate(). Running ...");
 			// Feedback:
@@ -75,24 +74,21 @@ bool CrazyflieAttitudeController::iterate()
 	return true;
 }
 
-void CrazyflieAttitudeController::gtposeCallback(const geometry_msgs::Pose::ConstPtr& msg)
-{
+void CrazyflieAttitudeController::gtposeCallback(const geometry_msgs::Pose::ConstPtr& msg){
 	m_GT_pose.position = msg->position;
 	m_GT_pose.orientation = msg->orientation;
 	if (!first_pose_received)
 			first_pose_received = true;
 }
 
-void CrazyflieAttitudeController::attitudeRefsCallback(const uned_crazyflie_controllers::AttitudeRefs::ConstPtr& msg)
-{
+void CrazyflieAttitudeController::attitudeRefsCallback(const uned_crazyflie_controllers::AttitudeRefs::ConstPtr& msg){
 	pitch_ref = msg->pitch;
 	roll_ref = msg->roll;
 	yaw_ref = msg->yaw;
 	if (!first_ref_received)
 			first_ref_received = true;
 }
-void CrazyflieAttitudeController::rateMixerRefsCallback(const double dpitch, const double droll, const double dyaw)
-{
+void CrazyflieAttitudeController::rateMixerRefsCallback(const double dpitch, const double droll, const double dyaw){
 	uned_crazyflie_controllers::RateMixerRefs ref_msg;
 
 	ref_msg.timestamp = ros::Time::now().toSec();
@@ -101,4 +97,70 @@ void CrazyflieAttitudeController::rateMixerRefsCallback(const double dpitch, con
 	ref_msg.dyaw = dyaw;
 
 	m_pub_control_signal.publish(ref_msg);
+}
+
+euler_angles CrazyflieAttitudeController::quaternion2euler(geometry_msgs::Quaternion quat){
+	euler_angles rpy;
+
+	// roll (x-axis rotation)
+	double sinr_cosp = 2 * (quat.w * quat.x + quat.y * quat.z);
+	double cosr_cosp = 1 - 2 * (quat.x * quat.x + quat.y * quat.y);
+	rpy.roll = std::atan2(sinr_cosp, cosr_cosp) * (180 / PI);
+
+	// pitch (y-axis rotation)
+	double sinp = 2 * (quat.w * quat.y - quat.z * quat.x);
+	if (std::abs(sinp) >= 1)
+	rpy.pitch = std::copysign(PI / 2, sinp) * (180 / PI);
+	else
+	rpy.pitch = std::asin(sinp) * (180 / PI);
+
+	// yaw (z-axis rotation)
+	double siny_cosp = 2 * (quat.w * quat.z + quat.x * quat.y);
+	double cosy_cosp = 1 - 2 * (quat.y * quat.y + quat.z * quat.z);
+	rpy.yaw = std::atan2(siny_cosp, cosy_cosp) * (180 / PI);
+
+	return rpy;
+}
+
+double CrazyflieAttitudeController::pid_controller(struct pid_s controller, double dt){
+	double outP = controller.kp * controller.error[0];
+	controller.integral = controller.integral + controller.ki * controller.error[1] * dt;
+	controller.derivative[0] = (controller.td/(controller.td+controller.nd+dt))*controller.derivative[1]+(controller.kd*controller.nd/(controller.td+controller.nd*dt))*(controller.error[0]-controller.error[1]);
+	double out = outP + controller.integral + controller.derivative[0];
+
+	if(controller.upperlimit != 0.0){
+		double out_i = out;
+
+		if (out > controller.upperlimit)
+			out = controller.upperlimit;
+		if (out < controller.lowerlimit)
+			out = controller.lowerlimit;
+
+		controller.integral = controller.integral - (out - out_i) * sqrt(controller.kp / controller.ki);
+	}
+
+	controller.error[1] = controller.error[0];
+	controller.derivative[1] = controller.derivative[0];
+
+	return out;
+}
+
+struct pid_s CrazyflieAttitudeController::init_controller(const char id[], double kp, double ki, double kd, double td, int nd, double upperlimit, double lowerlimit){
+	struct pid_s controller;
+
+	controller.kp = kp;
+	controller.ki = ki;
+	controller.kd = kd;
+	controller.td = td;
+	controller.nd = nd;
+	controller.error[0] = 0.0;
+	controller.error[1] = 0.0;
+	controller.integral = 0.0;
+	controller.derivative[0] = 0.0;
+	controller.derivative[1] = 0.0;
+	controller.upperlimit = upperlimit;
+	controller.lowerlimit = lowerlimit;
+
+	ROS_INFO("%s Controller: kp: %0.2f \tki: %0.2f \tkd: %0.2f", id, controller.kp, controller.ki, controller.kd);
+	return controller;
 }
