6c75806a585bf8a2eaf527c6304c0cb78c4f253c
FranciscoJManasAlvarez <fjmanas@dia.uned.es>
2022-08-28 15:21:56 +0200

    Update Crazyflie GUI

--- uned_crazyflie_gui/uned_crazyflie_gui/interface_node.py
+++ uned_crazyflie_gui/uned_crazyflie_gui/interface_node.py
@@ -1,6 +1,53 @@
-def main():
-    print('Hi from uned_crazyflie_gui.')
+import logging
+import time
+import rclpy
+import numpy as np
+import math
+import sys
+import os
 
+from rclpy.node import Node
+
+from qt_gui.settings import Settings
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+
+class MainWindow(QMainWindow):
+	def __init__(self, path):
+		#Qt Stuff..
+		super(MainWindow, self).__init__()
+		# Load the .ui file made from Qt designer
+		uic.loadUi(path, self)
+
+		print("Development in progress ...")
+
+## ^^^^^^^^^^^^^^^^^^^^^
+## Node
+## ^^^^^^^^^^^^^^^^^^^^^
+class InterfaceGUI(Node):
+    def __init__(self, aux):
+        super().__init__('interface')
+        self.initialize(aux)
+
+    def initialize(self, aux):
+        self.get_logger().info('InterfaceGUI::inicialize() ok.')
+        app = QApplication(aux)
+        path = os.path.dirname(os.path.abspath(__file__))
+        path = path.replace('\\', '/')+'/main.ui'
+        interfaz = MainWindow(path)
+        interfaz.show()
+        sys.exit(app.exec_())
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    interface_node = InterfaceGUI(sys.argv)
+    rclpy.spin(interface_node)
+
+    interface_node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
