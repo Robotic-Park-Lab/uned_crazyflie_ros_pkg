e4531fd2c32ee8316e2190ae0c305f7ebfc4335b
FranciscoJManasAlvarez <fjmanas@dia.uned.es>
2022-09-16 14:15:19 +0200

    Update. Swarm crazyflie in Webots

--- uned_crazyflie_webots/launch/robot_launch.py
+++ uned_crazyflie_webots/launch/robot_launch.py
@@ -9,9 +9,9 @@ from webots_ros2_driver.webots_launcher import WebotsLauncher
 
 def generate_launch_description():
     package_dir = get_package_share_directory('uned_crazyflie_webots')
-    dron01_description = pathlib.Path(os.path.join(package_dir, 'resource', 'crazyflie1.urdf')).read_text()
+    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'crazyflie1.urdf')).read_text()
     dron02_description = pathlib.Path(os.path.join(package_dir, 'resource', 'crazyflie2.urdf')).read_text()
-
+    
     webots = WebotsLauncher(
         world=os.path.join(package_dir, 'worlds', 'test.wbt')
     )
@@ -20,26 +20,21 @@ def generate_launch_description():
         package='webots_ros2_driver',
         executable='driver',
         output='screen',
+        name='dron01_driver',
+        additional_env={'WEBOTS_ROBOT_NAME': 'dron01'},
         parameters=[
-            {'robot_description': dron01_description,
+            {'robot_description': robot_description,
              'use_sim_time': True,
              'set_robot_state_publisher': True},
         ]
-    )
-
-    dron01_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[{
-            'robot_description': '<robot name=""><link name=""/></robot>'
-        }],
     )
 
     dron02_driver = Node(
         package='webots_ros2_driver',
         executable='driver',
         output='screen',
+        name='dron02_driver',
+        additional_env={'WEBOTS_ROBOT_NAME': 'dron02'},
         parameters=[
             {'robot_description': dron02_description,
              'use_sim_time': True,
@@ -47,7 +42,7 @@ def generate_launch_description():
         ]
     )
 
-    dron02_state_publisher = Node(
+    robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
@@ -59,9 +54,8 @@ def generate_launch_description():
     return LaunchDescription([
         webots,
         dron01_driver,
-        dron01_state_publisher,
+        robot_state_publisher,
         dron02_driver,
-        dron02_state_publisher,
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
