759356ee66292aa0547e10f6f9c6a4beec0535bb
FranciscoJManasAlvarez <fma527@ual.es>
2023-04-28 11:31:33 +0200

    Formation distributed

---
+++ uned_crazyflie_config/launch/demo_swarm_sphere_rbp.launch.py
@@ -0,0 +1,44 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    config_package_dir = get_package_share_directory('uned_crazyflie_config')
+    config_path = os.path.join(config_package_dir, 'resources', 'demo_swarm_formation_distance_five_sphere.yaml')
+    rviz_config_path = os.path.join(config_package_dir, 'rviz', 'sphere.rviz')
+
+    swarm_node = Node(
+        package='uned_crazyflie_driver',
+        executable='swarm_driver',
+        name='swarm',
+        output='screen',
+        shell=True,
+        emulate_tty=True,
+        parameters=[
+            {'config': config_path},
+            {'enviroment': 'swarm'},
+            {'robots': 'dron02, dron03'}
+        ]
+    )
+
+    rqt_node = Node(
+        package='rqt_gui',
+        executable='rqt_gui',
+        name='interface'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_path],
+    )
+
+    return LaunchDescription([
+        swarm_node,
+        rqt_node,
+        rviz_node
+    ])
