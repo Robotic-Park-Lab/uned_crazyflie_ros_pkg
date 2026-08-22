943a7473919f91aec1ba6f574e8084125b1836c8
FranciscoJManasAlvarez <fma527@ual.es>
2026-08-22 21:51:51 +0200

    Major clean

--- uned_crazyflie_config/launch/experience.launch.py
+++ uned_crazyflie_config/launch/experience.launch.py
@@ -27,19 +27,8 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 """
-Launch único parametrizado por experiencia (tarea 8 de la Fase 2, ver
-AUDIT.md en la rama doc). Sustituye a los 14+6 .launch.py de demo de
-uned_crazyflie_config/uned_crazyflie_webots: para lanzar una experiencia
-nueva basta con escribir un .yaml en resources/, no un .launch.py nuevo.
-
-Adaptado de RoboticPark/roboticpark_config/launch/experience.launch.py al
-alcance real de este repo (un único simulador -Webots-, sin
-mars_supervisor_pkg ni roboticpark_config, con una sección Missions nueva
-para lanzar nodos de uned_crazyflie_missions).
-
 Uso:
-    ros2 launch uned_crazyflie_config experience.launch.py \
-        config_file:=experience_swarm_teleop.yaml
+    ros2 launch uned_crazyflie_config experience.launch.py config_file:=experience_swarm_teleop.yaml
 
 Esquema del .yaml (ver resources/experience_*.yaml para ejemplos reales):
 
@@ -89,6 +78,7 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher, Ros2SupervisorLauncher
 from webots_ros2_driver.utils import controller_url_prefix
+from webots_ros2_driver.webots_controller import WebotsController
 
 DRIVER_URDF = {
     'pid': 'crazyflie.urdf',
@@ -100,68 +90,83 @@ def get_ros2_nodes(context, *args):
     node_list = []
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
 
+    #-------------------#
+    #     Load File     #
+    #-------------------#
     config_package_dir = get_package_share_directory('uned_crazyflie_config')
-    gui_package_dir = get_package_share_directory('uned_crazyflie_gui')
 
     file_name = LaunchConfiguration('config_file').perform(context)
     config_path = os.path.join(config_package_dir, 'resources', file_name)
     with open(config_path, 'r') as f:
         experience = yaml.safe_load(f)
 
-    # ---------------#
-    #  Simulation   #
-    # ---------------#
-    simulation = experience.get('Simulation', {'enable': False})
-    if simulation.get('enable', False):
-        webots_dir = get_package_share_directory('uned_crazyflie_webots')
-        webots = WebotsLauncher(
-            world=os.path.join(webots_dir, 'worlds', simulation['world']))
-        node_list.append(webots)
-        node_list.append(Ros2SupervisorLauncher())
-        node_list.append(Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{'robot_description': '<robot name=""><link name=""/></robot>'}],
-        ))
-        node_list.append(launch.actions.RegisterEventHandler(
-            event_handler=launch.event_handlers.OnProcessExit(
-                target_action=webots,
-                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+    #------------------------#
+    #     Operation mode     #
+    #------------------------#
+    if not experience['Operation']['mode'] == 'physical':
+        use_sim_time = True
+        if experience['Operation']['tool'] == 'Webots':
+            webots = WebotsLauncher(
+                world=os.path.join(config_package_dir, 'worlds', experience['Operation']['world']),
+                mode='realtime',
+                ros2_supervisor=False
             )
-        ))
+            node_list.append(webots)
+            # TO-DO: No funciona el ROS2Supervisor
+            # node_list.append(Ros2SupervisorLauncher())
+            
+            node_list.append(launch.actions.RegisterEventHandler(
+                event_handler=launch.event_handlers.OnProcessExit(
+                    target_action=webots,
+                    on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+                )
+            ))
+        elif experience['Operation']['tool'] == 'Gazebo':
+            world_path = os.path.join(config_path, 'worlds', experience['Operation']['world'])
+            gazebo = ExecuteProcess(cmd=['gazebo', '--verbose', world_path, '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', '--ros-args',
+                ], output='screen'
+            )
+            node_list.append(gazebo)
+            node_list.append(launch.actions.RegisterEventHandler(
+                event_handler=launch.event_handlers.OnProcessExit(
+                    target_action=webots,
+                    on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+                )
+            ))
 
     # -----------#
     #  Robots   #
     # -----------#
     physical_robots = []
     for robot_id, robot in experience.get('Robots', {}).items():
-        robot_type = robot['type']
+        if 'dron' in robot['name']:
+            robot_type = robot['type']
+            
+            if robot_type in ('virtual', 'digital_twin'):
+                urdf_name = os.path.join(config_package_dir, 'resources', 'crazyflie.urdf')
+                with open(urdf_name) as f:
+                    robot_desc = f.read()
+                aux = robot_desc.replace("dron00", robot['name'])
+                aux = aux.replace("name_id_value", robot['name'])
+                aux = aux.replace("CameraAlwayOn", str(robot['camera']['alwaysOn']))
+                aux = aux.replace("CameraEnable", str(robot['camera']['enable']))
+                aux = aux.replace("CameraUpdateRate", str(robot['camera']['update_rate']))
+                aux = aux.replace("config_file_path", config_path)
 
-        if robot_type in ('virtual', 'digital_twin'):
-            urdf_name = DRIVER_URDF[robot.get('driver', 'pid')]
-            webots_dir = get_package_share_directory('uned_crazyflie_webots')
-            with open(os.path.join(webots_dir, 'resources', urdf_name)) as f:
-                robot_description = f.read()
-            node_list.append(Node(
-                package='webots_ros2_driver',
-                executable='driver',
-                output='screen',
-                name=robot['name'],
-                additional_env={
-                    'WEBOTS_ROBOT_NAME': robot['name'],
-                    'WEBOTS_CONTROLLER_URL': controller_url_prefix() + robot['name'],
-                    'WEBOTS_ROBOT_CONFIG_FILE': os.path.join(
-                        config_package_dir, 'resources', robot['config_path']),
-                    'WEBOTS_ROBOT_ROLE': robot_type},
-                parameters=[{
-                    'robot_description': robot_description,
-                    'use_sim_time': use_sim_time,
-                    'set_robot_state_publisher': True}],
-            ))
+                robot_controller = WebotsController(
+                    robot_name=robot['name'],
+                    parameters=[
+                        {'robot_description': aux,
+                         'use_sim_time': use_sim_time,
+                         'set_robot_state_publisher': True},
+                    ],
+                    respawn=True
+                )
+                node_list.append(robot_controller)
 
-        if robot_type in ('physical', 'digital_twin'):
-            physical_robots.append(robot['name'])
+
+            if robot_type in ('physical', 'digital_twin'):
+                physical_robots.append(robot['name'])
 
     if physical_robots:
         node_list.append(Node(
@@ -177,39 +182,66 @@ def get_ros2_nodes(context, *args):
             ],
         ))
 
-    # --------------#
-    #  Interface   #
-    # --------------#
-    interface = experience.get('Interface', {})
-    rqt = interface.get('rqt', {'enable': False})
-    if rqt.get('enable', False):
-        # Los ficheros propios de uned_crazyflie_gui son genéricos; si no
-        # se encuentran ahí, se busca en uned_crazyflie_config/rqt (los
-        # ligados a una demo concreta).
-        rqt_path = os.path.join(gui_package_dir, 'rqt', rqt['file'])
-        if not os.path.isfile(rqt_path):
-            rqt_path = os.path.join(config_package_dir, 'rqt', rqt['file'])
+    #------------------------#
+    #     CPU Monitoring     #
+    #------------------------#
+    if experience['CPU_Monitoring']['enable']:
         node_list.append(Node(
-            package='rqt_gui',
-            executable='rqt_gui',
-            name='interface',
-            parameters=[{'use_sim_time': use_sim_time}],
-            arguments=['--perspective-file', rqt_path],
-        ))
-
-    rviz = interface.get('rviz', {'enable': False})
-    if rviz.get('enable', False):
-        rviz_path = os.path.join(gui_package_dir, 'rviz', rviz['file'])
-        if not os.path.isfile(rviz_path):
-            rviz_path = os.path.join(config_package_dir, 'rviz', rviz['file'])
-        node_list.append(Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
+            package=experience['CPU_Monitoring']['node']['pkg'],
+            executable=experience['CPU_Monitoring']['node']['executable'],
+            name=experience['CPU_Monitoring']['node']['name'],
             output='screen',
-            arguments=['-d', rviz_path],
-            parameters=[{'use_sim_time': use_sim_time}],
+            parameters=[{
+                'process_name' : experience['CPU_Monitoring']['processes'],
+                'process_period' : 0.5},
+            ],
         ))
+    
+    #--------------------#
+    #     Interfaces     #
+    #--------------------#
+    interface = experience.get('Interface', {})
+    if interface['enable']:
+        if interface['rqt']['enable']:
+            rqt_config_path = os.path.join(config_package_dir, 'rqt', interface['rqt']['file'])
+            node_list.append(Node(
+                package=interface['rqt']['node']['pkg'],
+                executable=interface['rqt']['node']['executable'],
+                name=interface['rqt']['node']['name'],
+                parameters=[
+                    {'use_sim_time': use_sim_time},
+                ],
+                arguments=['--perspective-file', rqt_config_path],
+            ))
+        if interface['rviz2']['enable']:
+            rviz_config_path = os.path.join(config_package_dir, 'rviz', interface['rviz2']['file'])
+            node_list.append(Node(
+                package=interface['rviz2']['node']['pkg'],
+                executable=interface['rviz2']['node']['executable'],
+                name=interface['rviz2']['node']['name'],
+                parameters=[
+                    {'use_sim_time': use_sim_time},
+                ],
+                arguments=['-d', rviz_config_path],
+            ))
+            node_list.append(Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                output='screen',
+                name='RoboticPark',
+                arguments=['--yaw', '3.1415', '--frame-id', 'RoboticPark/base_link', '--child-frame-id', 'map'],
+            ))
+        if interface['own']['enable']:
+            own_config_path = os.path.join(config_package_dir, 'resources', interface['own']['file'])
+            node_list.append(Node(
+                package=interface['own']['node']['pkg'],
+                executable=interface['own']['node']['executable'],
+                name=interface['own']['node']['name'],
+                parameters=[
+                    {'use_sim_time': use_sim_time},
+                ],
+                arguments=['-d', own_config_path],
+            ))
 
     # -----------------#
     #  Data Logging   #
@@ -224,6 +256,7 @@ def get_ros2_nodes(context, *args):
         else:
             cmd = ['ros2', 'bag', 'record', '-o', bag_name, data_logging.get('topics', '')]
         node_list.append(ExecuteProcess(cmd=cmd, output='screen', shell=True))
+
 
     # --------------#
     #  Missions    #
